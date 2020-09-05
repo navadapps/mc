e2aa0717e055e7fcd16e45650ab396e95b36e225
navadapps <70310378+navadapps@users.noreply.github.com>
2020-09-05 10:49:47 -0400

    Updated
    Made some changes

--- order-service/src/main/java/io/navadappsMC/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/io/navadappsMC/orderservice/OrderServiceApplication.java
@@ -2,10 +2,25 @@ package io.navadappsMC.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderServiceApplication {
 
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
+	
+	@Bean
+	public WebClient.Builder getWebClientBuilder() {
+		return WebClient.builder();
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(OrderServiceApplication.class, args);
 	}
